c0b272d505e66a1333c5ec609c85e9304d8fd0e0
Andrew Chen <deweichen47@gmail.com>
2014-08-10 19:02:57 -0700

    lil bit changes

--- static/mltest/RegressionLine.js
+++ static/mltest/RegressionLine.js
@@ -1,18 +1,20 @@
-var RegressionLine = function(theta, mu, sigma, max) {
+var RegressionLine = function(theta, mu, sigma, min, max) {
 
 	this.theta = theta;
 	this.mu = mu[0];
 	this.sigma = sigma[0];
+	this.min = min;
 	this.max = max;
 	this.regLine = [];
 };
 
 RegressionLine.prototype.create = function() {
-	var norm_x1 = (0 - this.mu) / this.sigma;
+	var norm_x1 = (this.min - this.mu) / this.sigma;
 	var norm_x2 = (this.max - this.mu) / this.sigma;
 
-	this.regLine.push([0, (this.theta[0] + this.theta[1] * norm_x1)]);
-	this.regLine.push([this.max, (this.theta[0] + this.theta[1] * norm_x2)]);
+	// We just need 2 points to plot the line
+	this.regLine.push([this.min, this.theta[0] + this.theta[1] * norm_x1]);
+	this.regLine.push([this.max, this.theta[0] + this.theta[1] * norm_x2]);
 
 	return this.regLine;
 };
