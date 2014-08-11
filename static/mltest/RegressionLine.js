15fa989478c047795983b275ea2063412cf6d039
Andrew Chen <deweichen47@gmail.com>
2014-08-10 18:43:41 -0700

    final clean up

--- static/mltest/RegressionLine.js
+++ static/mltest/RegressionLine.js
@@ -1,22 +1,18 @@
-var RegressionLine = function(theta, mu, sigma, range) {
+var RegressionLine = function(theta, mu, sigma, max) {
 
-	this.theta = $(theta);
+	this.theta = theta;
 	this.mu = mu[0];
 	this.sigma = sigma[0];
-	this.range = $(range);
+	this.max = max;
 	this.regLine = [];
 };
 
 RegressionLine.prototype.create = function() {
 
-	// console.log(this.mu[0]);
-	// console.log(this.sigma[0]);
+	var norm_x = (this.max - this.mu) / this.sigma;	// Normalized x
 
-	for (var i = this.range[0]; i < this.range[1]; i++) {
-		var norm_x = (i - this.mu) / this.sigma;	// Normalized x
-		var point = [i, (this.theta[0] + this.theta[1] * norm_x)];
-		this.regLine.push(point);
-	};
+	this.regLine.push([0, 0]);
+	this.regLine.push([this.max, (this.theta[0] + this.theta[1] * norm_x)]);
 
 	return this.regLine;
 };
